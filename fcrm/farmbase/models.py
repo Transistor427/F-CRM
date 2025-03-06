6c8fe9a1e142c201020fa46cf54bc35f47e1d901
Vlad <427departament@gmail.com>
2025-03-06 18:07:13 +0300

    Initial commit

--- fcrm/farmbase/models.py
+++ fcrm/farmbase/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Parts(models.Model):
+    name = models.CharField(max_length=100)
+    module = models.CharField(max_length=100)
+    quantity = models.IntegerField(default=0)
+    image = models.ImageField(upload_to=None, height_field=500, width_field=500, max_length=100)
